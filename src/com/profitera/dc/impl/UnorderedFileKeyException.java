011d69841c5ed96ea587fac094fe9652b257651e
Josh Uzo <nwankwo.joshua@gmail.com>
2018-09-17 00:00:00 +1000

    Migration & updates

---
+++ src/com/profitera/dc/impl/UnorderedFileKeyException.java
@@ -0,0 +1,8 @@
+package com.profitera.dc.impl;
+
+public class UnorderedFileKeyException extends Exception {
+  private static final long serialVersionUID = 1L;
+  public UnorderedFileKeyException(String message) {
+    super(message);
+  }
+}
