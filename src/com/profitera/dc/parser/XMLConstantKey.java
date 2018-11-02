592a17c5f53a984212b4ab20f2f904552b9491c6
Josh Uzo <nwankwo.joshua@gmail.com>
2018-11-03 00:00:00 +1100

    Migration & updates

---
+++ src/com/profitera/dc/parser/XMLConstantKey.java
@@ -0,0 +1,62 @@
+package com.profitera.dc.parser;
+
+public interface XMLConstantKey {
+  public static final String XML_DOC = "doc";
+	public static final String XML_LOAD_KEY = "load"; 
+	public static final String XML_GENERATED_KEY_KEY = "generatedkey"; 
+	public static final String XML_GENERATED_KEY_SEQ_NAME_KEY = "generatedkeyseqname";
+	public static final String XML_SPREADSHEET = "sheet";
+	public static final String XML_XML_START_TAG = "recordindicatortag";
+	public static final String XML_XQUERY = "xquery";
+	public static final String XML_TABLE_KEY = "table";
+	public static final String XML_DELIMITER_KEY = "delimiter";
+	public static final String XML_ASCII_DELIMITER_KEY = "asciidelimiter";
+	public static final String XML_PAD_LINE_KEY = "padline"; 
+	public static final String XML_KEY	= "key";
+	public static final String XML_INSERT_KEY_KEY	= "insertkey";
+	public static final String XML_REFRESH_DATA_KEY = "refreshdata"; 
+	public static final String XML_REFRESH_DATA_QUERY_TIMEOUT = "timeout"; 
+	public static final String XML_HEADER_KEY = "header"; 
+	public static final String XML_TRAILER_KEY = "trailer";
+	public static final String XML_VALUE_KEY = "value";
+	
+	public static final String XML_QUERY_WHERE_KEY = "querywhere"; 
+	public static final String XML_SELECT_KEY = "select";
+	public static final String XML_UPDATE_KEY = "update";
+	public static final String XML_POST_KEY = "post";
+	
+	public static final String XML_INSERT_UPDATE_KEY = "insertupdate"; 
+	public static final String XML_INSERT_INSERTION_KEY = "insertinsertion"; 
+	public static final String XML_UPDATE_UPDATE_KEY = "updateupdate";
+	public static final String XML_UPDATE_INSERTION_KEY = "updateinsertion";
+	public static final String XML_IS_OPTIONAL_SUFFIX = "isoptional";
+	public static final String XML_IS_EXTERNAL_SUFFIX = "isexternal";
+	public static final String XML_IS_KEY_SUFFIX = "iskey"; 
+	
+	public static final String XML_LOCATION_SUFFIX = "location"; 
+	public static final String XML_LOCATION_START = "start"; 
+	public static final String XML_LOCATION_END = "end";
+	public static final String XML_HANDLER_SUFFIX = "handler"; 
+	public static final String XML_HANDLER_ARGS_SUFFIX = "config"; 
+	public static final String XML_FILTER_ON_SUFFIX = "filteron"; 
+	public static final String XML_NULL_DEFINITION_SUFFIX = "nulldefinition"; 
+	public static final String XML_DEFAULT_SUFFIX = "default";
+	public static final String XML_LOOKUP_KEY = "lookup";
+	public static final String XML_TYPE_KEY = "type";
+	public static final String XML_QUERY_KEY = "query";
+	public static final String XML_INSERT_KEY = "insert";
+	public static final String XML_CACHE = "cache";
+	public static final String XML_CACHE_CAPACITY = "cachecapacity";
+	 
+	public static final String XML_FIELD_KEY	= "field";
+	public static final String XML_FIELD_NAME = "name";
+	
+	public static final String XML_FULL_CACHE_KEY = "fullcache";
+	public static final String XML_VERIFY_UNIQUENESS_KEY = "verifyfilekey";
+	public static final String XML_MODE_KEY = "mode";
+	public static final String XML_HANDLER_NAME = "name";
+  public static final String REMARKS = "remarks";
+  //
+  public static final String FILTERS = "filters";
+  public static final String FILTER = "filter";
+}
